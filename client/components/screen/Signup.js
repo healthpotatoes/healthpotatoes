21a982950ebc41c616b99d0a47fa0f6f970aaf79
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-29 14:58:01 -0400

    added some text

--- client/components/screen/Signup.js
+++ client/components/screen/Signup.js
@@ -2,3 +2,13 @@ import React from "react";
 import {StyleSheet, Text, View}from "react-native";
 
 
+const Signup=()=>{
+    return(
+        <View>
+            <Text>Signup
+                </Text>
+            </View>
+
+    )
+}
+export default Signup;
