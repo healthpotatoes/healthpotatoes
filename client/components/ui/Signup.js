21b052c1abd33d107fbda734243b419aa75aece3
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-30 22:07:29 -0400

    added sign up page code

--- client/components/ui/Signup.js
+++ client/components/ui/Signup.js
@@ -1,18 +1,27 @@
 import React from "react"; 
-import {StyleSheet, Text, View, TextInput}from "react-native";
+// import {StyleSheet, Text, View, TextInput}from "react-native";
+import { Container, Header, Content, Form, Item, Input, Text, Label } from 'native-base';
+      
 
-
-const SignupPage=()=>{
-    return(
-        <View>
-            <Text>Signup
-                </Text>
-
-                <Text>Name</Text>
-                <TextInput
-                />
-            </View>
-
-    )
-}
-export default SignupPage;
+    export default class SignupPage extends Component {
+          render() {
+            return (
+              <Container>
+                <Header />
+                <Content>
+                  <Form>
+                      <Text>Sign-Up</Text>
+                    <Item stackedLabel>
+                      <Label>Username</Label>
+                      <Input />
+                    </Item>
+                    <Item stackedLabel last>
+                      <Label>Password</Label>
+                      <Input />
+                    </Item>
+                  </Form>
+                </Content>
+              </Container>
+            );
+          }
+        }
