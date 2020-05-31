9e515e327e9d464bb8382f72078e697142ad0f47
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-30 22:10:57 -0400

    added sign up button

--- client/components/ui/Signup.js
+++ client/components/ui/Signup.js
@@ -1,27 +1,39 @@
-import React from "react"; 
+import React from 'react';
 // import {StyleSheet, Text, View, TextInput}from "react-native";
-import { Container, Header, Content, Form, Item, Input, Text, Label } from 'native-base';
-      
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Text,
+  Button,
+  Label,
+} from 'native-base';
 
-    export default class SignupPage extends Component {
-          render() {
-            return (
-              <Container>
-                <Header />
-                <Content>
-                  <Form>
-                      <Text>Sign-Up</Text>
-                    <Item stackedLabel>
-                      <Label>Username</Label>
-                      <Input />
-                    </Item>
-                    <Item stackedLabel last>
-                      <Label>Password</Label>
-                      <Input />
-                    </Item>
-                  </Form>
-                </Content>
-              </Container>
-            );
-          }
-        }
+export default class SignupPage extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Text>Sign-Up</Text>
+            <Item stackedLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+            <Button rounded info>
+              <Text>Sign-in</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
