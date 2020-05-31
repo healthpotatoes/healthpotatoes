c1d51f02c693bbeade5cddd44b241ca25f53031c
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-30 21:31:33 -0400

    added native base

---
+++ client/components/ui/Signup.js
@@ -0,0 +1,18 @@
+import React from "react"; 
+import {StyleSheet, Text, View, TextInput}from "react-native";
+
+
+const SignupPage=()=>{
+    return(
+        <View>
+            <Text>Signup
+                </Text>
+
+                <Text>Name</Text>
+                <TextInput
+                />
+            </View>
+
+    )
+}
+export default SignupPage;
