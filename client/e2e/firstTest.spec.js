902f4285028110890a22f65260e6e94c0b4d760f
Shane Quick <shanesfast@gmail.com>
2020-06-17 14:51:11 -0500

    Add simple test for baseline Travis CI integration

--- client/e2e/firstTest.spec.js
+++ client/e2e/firstTest.spec.js
@@ -1,11 +1,11 @@
 // Example for Detox testing
 
-// describe('App', () => {
-//   beforeEach(async () => {
-//     await device.reloadReactNative();
-//   });
+describe('Truth', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
 
-//   it('should show the step one message', async () => {
-//     await expect(element(by.id('stepOne'))).toBeVisible();
-//   });
-// });
+  it('is true', async () => {
+    await expect(true).toEqual(true);
+  });
+});
